28f6e126440e331e36e2f2c4e37efd7b14771d45
ppoklafke <blinderbg@gmail.com>
2025-08-03 02:38:09 -0300

    feat: Adicionar configurações de deploy e preview

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,18 +5,12 @@ module.exports = {
     args: 'run preview',
     cwd: '/home/ubuntu/jpedidos-dark-blue-74',
     instances: 1,
-    exec_mode: 'fork',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
       PORT: 8080
-    },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true,
-    autorestart: true,
-    max_restarts: 10,
-    min_uptime: '10s',
-    max_memory_restart: '1G'
+    }
   }]
 };
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,16 @@ export default defineConfig(({ mode }) => ({
       usePolling: true, // Necessário para hot reload no Docker
     },
   },
+
+preview: {
+    host: true, // Ouve em todos os IPs, incluindo 127.0.0.1
+    port: 8080, // A porta que o Nginx está usando
+    strictPort: true,
+    // A LINHA MAIS IMPORTANTE QUE RESOLVE O PROBLEMA
+    allowedHosts: ["jpedidos.klafkefilho.com.br"],
+  },
+
+
   plugins: [
     react(),
     mode === 'development' &&
